bdbde4196e74e4bca20e661223d48be5a1551f1e
rabinpoudyal1995 <rabinpoudyal1995@gmail.com>
2018-09-25 08:14:35 +0545

    Functions and packages

---
+++ 11-Functions.R
@@ -0,0 +1,16 @@
+# Functions in R programming language
+# rnorm(), c(), seq(), rep(), print()
+is.numeric()
+is.integer()
+is.double()
+is.character()
+typeof()
+
+sqrt()
+paste()
+
+# ? before function brings help for function
+?rnorm()
+
+# Get 100 numbers from 10 to 20 
+seq(from=10, to=20, length.out = 100)
---
+++ 12-packages.R
@@ -0,0 +1,7 @@
+# This is how we install packages
+install.packages("ggplot2")
+
+# you need to activate the package before using it
+library(ggplot)
+?ggplot()
+?diamonds
